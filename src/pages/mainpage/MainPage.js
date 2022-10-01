24025e582d20ed6a0228824ee1741ffbe65be856
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-10-01 21:05:51 +0530

    react app created

---
+++ src/pages/mainpage/MainPage.js
@@ -0,0 +1,12 @@
+import './mainpage.css';
+
+
+const MainPage = () => {
+ const api={
+  key:"f717ff242248a3b5708565b749b90eed"
+  base:"https://api.openweathermap.org/data/2.5/"
+ }
+  return <div></div>;
+};
+
+export default MainPage;
