fd616316e786f1580ec2d79fdea266e80b6b4ac2
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-10-13 23:58:34 +0530

    components re-structured, api is called by clicking.

---
+++ src/components/form/FormComp.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { useState } from 'react';
+import './form.css';
+
+const FormComp = () => {
+  const [place, setPlace] = useState('');
+
+  const apiKey = 'f717ff242248a3b5708565b749b90eed';
+
+  const handleChange = (e) => {
+    setPlace({ ...place, [e.target.name]: e.target.value });
+  };
+
+  const callApi = async () => {
+    try {
+      const data = await axios
+        .get(
+          `http://api.openweathermap.org/geo/1.0/direct?q= ${place.city},${place.country}&appid=${apiKey}`
+        )
+        .then((res) => {
+          handleSubmit(res.data[0].lat, res.data[0].lon);
+          console.log(res.data[0].lat);
+        });
+    } catch (err) {
+      console.log('err');
+    }
+  };
+
+  const handleSubmit = (lat, lon) => {
+    try {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        )
+        .then((res) => {
+          console.log(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            callApi();
+          }}
+        >
+          <input
+            type='text'
+            placeholder='country'
+            name='country'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            placeholder='city'
+            name='city'
+            onChange={handleChange}
+          />
+          <button type='submit'>search</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormComp;
--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,49 +1,9 @@
 import './mainpage.css';
-import Form from '../../components/form/Form';
-import axios from 'axios';
-
-import { useState, useEffect } from 'react';
-
+import FormComp from '../../components/form/FormComp';
 const MainPage = () => {
-  const apiKey = 'f717ff242248a3b5708565b749b90eed';
-
-  const [locationLat, setLocationLat] = useState('');
-  const [locationLon, setLocationLon] = useState('');
-
-  const [onChange, setOnChange] = useState('');
-
-  const handleChange = (e) => {
-    setOnChange({ ...onChange, [e.target.name]: e.target.value });
-  };
-  console.log(onChange.city);
-  const callApi = async () => {
-    try {
-      const data = await axios
-        .get(
-          `http://api.openweathermap.org/geo/1.0/direct?q= ${onChange.city},${onChange.country}&appid=${apiKey}`
-        )
-        .then((res) => {
-          setLocationLat(res.data.lat);
-          setLocationLon(res.data.lon);
-          console.log(res.data);
-        });
-    } catch (err) {
-      console.log('err');
-    }
-  };
-
-  useEffect(() => {
-    callApi();
-  }, [onChange]);
-
   return (
     <div>
-      <Form
-        lat={locationLat}
-        lon={locationLon}
-        handleChange={handleChange}
-        apiKey={apiKey}
-      />
+      <FormComp />
     </div>
   );
 };
